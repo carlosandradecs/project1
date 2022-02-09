c1e53f96b9f1b7f8cce7ceb45d4fe91298816236
carlosandradecs <79880667+carlosandradecs@users.noreply.github.com>
2022-02-08 19:32:08 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "defns.h"
+int main() {
+    symbol* sy;
+    symbol* Symbols= new symbol[NSYMBOLS];
+    sy->freq=0;
+    sy->parent=NULL;
+    sy->left=NULL;
+    sy->right=NULL;
+    
+
+
+}
